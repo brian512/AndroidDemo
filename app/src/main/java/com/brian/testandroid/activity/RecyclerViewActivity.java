c92e7cd35125cc78be7b65dc5ada691ea7141fbf
brian512 <huamm512@163.com>
2016-11-25 20:16:55 +0800

    update horizontal recycleview

--- app/src/main/java/com/brian/testandroid/activity/RecyclerViewActivity.java
+++ app/src/main/java/com/brian/testandroid/activity/RecyclerViewActivity.java
@@ -5,6 +5,8 @@ import android.view.View;
 
 import com.brian.testandroid.R;
 import com.brian.testandroid.common.BaseActivity;
+import com.brian.testandroid.util.DeviceUtil;
+import com.brian.testandroid.util.LogUtil;
 import com.brian.testandroid.util.ToastUtil;
 import com.brian.testandroid.view.swiperecycleview.DefaultItemTouchHelper;
 import com.brian.testandroid.view.swiperecycleview.HorizontalRecyclerView;
@@ -26,8 +28,6 @@ public class RecyclerViewActivity extends BaseActivity {
     @BindView(R.id.recycler_view)
     HorizontalRecyclerView mRecyclerView;
 
-    private HorizontalRecyclerView.InnerAdapter mAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,38 +36,41 @@ public class RecyclerViewActivity extends BaseActivity {
 
         //创建默认的线性LayoutManager
         mRecyclerView.setHasFixedSize(true); // 如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
+        mRecyclerView.setPadding(DeviceUtil.getScreenWidth(this)/2, 0, DeviceUtil.getScreenWidth(this)/2, 0);
+        mRecyclerView.setClipToPadding(false);
+        mRecyclerView.getAdapter().bindDatas(getDummyDatas());
 //        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
-        mAdapter = new HorizontalRecyclerView.InnerAdapter(getDummyDatas()); // 创建并设置Adapter
-        mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setItemTouchHelperListener(new DefaultItemTouchHelper.OnItemTouchCallbackListener() {
 
             @Override
             public void onSwiped(int adapterPosition) {
-                mAdapter.getDatas().remove(adapterPosition);
-                mAdapter.notifyItemRemoved(adapterPosition);
+                mRecyclerView.getAdapter().getDatas().remove(adapterPosition);
+                mRecyclerView.getAdapter().notifyItemRemoved(adapterPosition);
             }
 
             @Override
             public boolean onMove(int srcPosition, int targetPosition) {
                 // 更换数据源中的数据Item的位置
-                Collections.swap(mAdapter.getDatas(), srcPosition, targetPosition);
+                Collections.swap(mRecyclerView.getAdapter().getDatas(), srcPosition, targetPosition);
 
                 // 更新UI中的Item的位置，主要是给用户看到交互效果
-                mAdapter.notifyItemMoved(srcPosition, targetPosition);
+                mRecyclerView.getAdapter().notifyItemMoved(srcPosition, targetPosition);
                 return true;
             }
         }, false, false);
-        mAdapter.setOnItemClickLitener(new HorizontalRecyclerView.OnItemClickLitener() {
+        mRecyclerView.setOnItemSelectedListener(new HorizontalRecyclerView.OnItemSelectedLitener() {
             @Override
-            public void onItemClick(View view, int position) {
-                ToastUtil.showMsg("" + mAdapter.getDatas().get(position));
+            public void onItemSelected(View view, int position) {
+                mRecyclerView.smoothToCenter(view);
+                ToastUtil.showMsg("position=" + position + "; data=" + mRecyclerView.getAdapter().getDatas().get(position));
+                LogUtil.d("position=" + position + "; data=" + mRecyclerView.getAdapter().getDatas().get(position));
             }
         });
     }
 
     private List<String> getDummyDatas() {
         List<String> datas = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 10; i++) {
             datas.add("data:" + i);
         }
         return datas;
--- app/src/main/java/com/brian/testandroid/view/swiperecycleview/HorizontalRecyclerView.java
+++ app/src/main/java/com/brian/testandroid/view/swiperecycleview/HorizontalRecyclerView.java
@@ -9,13 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Scroller;
 
 import com.brian.testandroid.R;
+import com.brian.testandroid.util.LogUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
  * Created by huamm on 2016/11/10 0010.
  */
 
@@ -23,20 +25,132 @@ public class HorizontalRecyclerView extends RecyclerView {
 
     private DefaultItemTouchHelper mItemTouchHelper;
 
+    /**
+     * Item点击监听
+     */
+    private OnItemSelectedLitener mOnItemSelectedLitener;
+
+    private Scroller mScroller;
+    private int mLastx;
+
+    private InnerAdapter mAdapter;
+
     public HorizontalRecyclerView(Context context) {
         this(context, null, 0);
     }
+
     public HorizontalRecyclerView(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
+
     public HorizontalRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mScroller = new Scroller(context);
+        mAdapter = new InnerAdapter(null);
+        setAdapter(mAdapter);
 
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(context);
 
         mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL); // 横向列表
         setLayoutManager(mLayoutManager);
 
+        addOnScrollListener(new OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    smoothToCenter(mCenterIndex);
+                    LogUtil.d("mCenterIndex=" + mCenterIndex);
+                    View child = getChildAt(mCenterIndex);
+                    mOnItemSelectedLitener.onItemSelected(child, getChildAdapterPosition(child));
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (getWidth() <= 0) return;
+
+                float maxScale = 0;
+                for (int i = 0, count = getChildCount(); i < count; i++) {
+                    View view = getChildAt(i);
+                    float scale = 1.2f - Math.abs((view.getLeft() + view.getRight()) / 2 - getWidth() / 2) * 1f / getWidth();
+
+                    view.setScaleX(scale);
+                    view.setScaleY(scale);
+
+                    if (scale > maxScale) {
+                        maxScale = scale;
+                        mCenterIndex = i;
+                    }
+                }
+            }
+        });
+    }
+
+    public InnerAdapter getAdapter() {
+        return mAdapter;
+    }
+
+    public void setOnItemSelectedListener(OnItemSelectedLitener listener) {
+        mOnItemSelectedLitener = listener;
+        mAdapter.setOnItemSelectedListener(mOnItemSelectedLitener);
+    }
+
+    private int mCenterIndex = 0;
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+
+        if (mScroller != null && mScroller.computeScrollOffset()) {
+            scrollBy(mLastx - mScroller.getCurrX(), 0);
+            mLastx = mScroller.getCurrX();
+            postInvalidate();//让系统继续重绘，则会继续重复执行computeScroll
+        }
+    }
+
+    /**
+     * 这是库的bug，设置padding后，获取可见第一项的下标会错乱
+     * <url>https://code.google.com/p/android/issues/detail?id=181412<url/>
+     * Issue 181412:   LinearLayoutManager findFirstVisibleItemPosition() with setClipToPadding(false);
+     */
+    public void smoothToCenter(View view) {
+        for (int i=0, count = getChildCount(); i < count; i++) {
+            if (getChildAt(i) == view) {
+                smoothToCenter(i);
+                break;
+            }
+        }
+    }
+
+    /**
+     * 将指定item平滑移动到整个view的中间位置
+     */
+    public void smoothToCenter(int position) {
+        int parentWidth = getWidth();//获取父视图的宽度
+        //获取可视范围内的选项的头尾位置
+        View targetChild = getChildAt(position);//获取目标item在当前可见视图item集合中的位置
+        if (targetChild == null) {
+            return;
+        }
+        int childLeftPx = targetChild.getLeft();//子view相对于父view的左边距
+        int childRightPx = targetChild.getRight();//子view相对于父view的右边距
+
+        int childWidth = targetChild.getWidth();
+        int centerLeft = parentWidth / 2 - childWidth / 2;//计算子view居中后相对于父view的左边距
+        int centerRight = parentWidth / 2 + childWidth / 2;//计算子view居中后相对于父view的右边距
+        if (childLeftPx > centerLeft) {//子view左边距比居中view大（说明子view靠父view的右边，此时需要把子view向左平移
+            //平移的起始位置就是子view的左边距，平移的距离就是两者之差
+            mLastx = childLeftPx;
+            mScroller.startScroll(childLeftPx, 0, centerLeft - childLeftPx, 0, 500);//200为移动时长，可自行设定
+            postInvalidate();
+        } else if (childRightPx < centerRight) {
+            mLastx = childRightPx;
+            mScroller.startScroll(childRightPx, 0, centerRight - childRightPx, 0, 500);
+            postInvalidate();
+        }
     }
 
     public void setItemTouchHelperListener(DefaultItemTouchHelper.OnItemTouchCallbackListener listener, boolean dragable, boolean swipeable) {
@@ -47,20 +161,41 @@ public class HorizontalRecyclerView extends RecyclerView {
     }
 
     public static class InnerAdapter extends RecyclerView.Adapter<InnerAdapter.ViewHolder> {
-        public List<String> datas = null;
+        public List<String> mDatas = null;
 
-        /**
-         * Item点击监听
-         */
-        private OnItemClickLitener mOnItemClickLitener;
-
+        private OnItemSelectedLitener mOnItemSelectedLitener;
 
         public InnerAdapter(List<String> datas) {
-            this.datas = datas;
+            mDatas = new ArrayList<>();
+            bindDatas(datas);
         }
 
         public List<String> getDatas() {
-            return datas;
+            return mDatas;
+        }
+
+        public void bindDatas(List<String> datas) {
+            if (datas == null) {
+                return;
+            }
+            if (mDatas == null) {
+                mDatas = new ArrayList<>();
+            } else {
+                mDatas.clear();
+            }
+            mDatas.addAll(datas);
+        }
+
+        public void addDatas(List<String> datas) {
+            if (mDatas == null) {
+                mDatas = new ArrayList<>();
+            }
+            mDatas.addAll(datas);
+            notifyDataSetChanged();
+        }
+
+        public boolean isEmpty() {
+            return mDatas == null || mDatas.isEmpty();
         }
 
         //创建新View，被LayoutManager所调用
@@ -79,15 +214,19 @@ public class HorizontalRecyclerView extends RecyclerView {
             viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    mOnItemClickLitener.onItemClick(viewHolder.itemView, viewHolder.getAdapterPosition());
+                    mOnItemSelectedLitener.onItemSelected(viewHolder.itemView, viewHolder.getAdapterPosition());
                 }
             });
+        }
+
+        public void setOnItemSelectedListener(OnItemSelectedLitener listener) {
+            mOnItemSelectedLitener = listener;
         }
 
         //获取数据的数量
         @Override
         public int getItemCount() {
-            return datas == null ? 0 : datas.size();
+            return mDatas == null ? 0 : mDatas.size();
         }
 
         //自定义的ViewHolder，持有每个Item的的所有界面元素
@@ -99,14 +238,10 @@ public class HorizontalRecyclerView extends RecyclerView {
                 mTextView = (ImageView) view.findViewById(R.id.item_title);
             }
         }
-
-        public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener) {
-            this.mOnItemClickLitener = mOnItemClickLitener;
-        }
     }
 
-    public interface OnItemClickLitener {
-        void onItemClick(View view, int position);
+    public interface OnItemSelectedLitener {
+        void onItemSelected(View view, int position);
     }
 
 }
